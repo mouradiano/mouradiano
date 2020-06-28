b9eff3a471dafac38be10dc026e504a7acb28a0d
mouradiano <67483967+mouradiano@users.noreply.github.com>
2020-06-28 11:17:18 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+/*var myelement =document.getElementById('mydiv'),
+mybody=document.getElementById('body');
+if(screen.width<=768){
+  
+  mydiv.classList.remove('mena')
+  console.log('welcome')
+}
+
+*/
